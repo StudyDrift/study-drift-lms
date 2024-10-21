09b508f71591b7f7ca40ff41176630f7fcd66837
Chase Willden <chase.willden@gmail.com>
2024-10-20 18:01:35 -0600

    Fix calendar from package upgrade

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import * as React from "react"
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
+import * as React from "react"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -60,8 +60,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        PreviousMonthButton: ({ ...props }) => (
+          <ChevronLeftIcon className="h-4 w-4" />
+        ),
+        NextMonthButton: ({ ...props }) => (
+          <ChevronRightIcon className="h-4 w-4" />
+        ),
       }}
       {...props}
     />
